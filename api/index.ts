473b119d083aed0e809f81dca7d04393e87a84f8
Juan Pujol <jp@beakyn.com>
2020-08-04 12:45:07 -0300

    refactor: improve img param with url error message

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ import * as fetch from 'node-fetch';
 export default async (req: NowRequest, res: NowResponse) => {
   const {img, r, w} = req.query;
 
-  if (!img) return res.status(400).json({error: 'img url missing'})
+  if (!img) return res.status(400).json({code: 400, error: 'img param with url missing.'})
 
   try {
     const fetchResponse = await fetch(img)
