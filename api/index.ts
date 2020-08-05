ae60a1685383ecfa68968699f0adf05a6ede6b06
Juan Pujol <jp@beakyn.com>
2020-08-05 20:17:20 -0300

    feat(api): improve resize functions
    Closes #2

--- api/index.ts
+++ api/index.ts
@@ -3,9 +3,17 @@ import sharp from 'sharp';
 import * as fetch from 'node-fetch';
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const {img, r, w} = req.query;
+  const {img, r, w, h, f, pos, bg, we} = req.query
 
   if (!img) return res.status(400).json({code: 400, error: 'img param with url missing.'})
+
+  /**
+   * Normalize params
+   */
+  const fit = f || 'cover'
+  const background = bg ? `#${bg}` : '#00000000'
+  const position = String(pos).replace(',', ' ') || 'center'
+  const withoutEnlargement = Boolean(we)
 
   try {
     const fetchResponse = await fetch(img)
@@ -13,8 +21,18 @@ export default async (req: NowRequest, res: NowResponse) => {
 
     let sharpResponse = sharp(buffer)
 
-    if (r) sharpResponse = sharpResponse.rotate(Number(r) || 0, {background: '#00000000'})
-    if (w) sharpResponse = sharpResponse.resize({width: Number(w)});
+    if (w || h) sharpResponse = sharpResponse.resize(
+      Number(w) || undefined,
+      Number(h) || undefined,
+      {
+        fit,
+        position,
+        background,
+        withoutEnlargement,
+      }
+    );
+
+    if (r) sharpResponse = sharpResponse.rotate(Number(r) || 0, { background })
 
     sharpResponse = await sharpResponse.png().toBuffer()
 
