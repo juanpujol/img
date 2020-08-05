0afa4acb920c51bbd5b0ecc727777f1677b4153a
Juan Pujol <jp@beakyn.com>
2020-08-05 20:24:08 -0300

    fix(api): use `fit` instad of `f` param

--- api/index.ts
+++ api/index.ts
@@ -3,14 +3,13 @@ import sharp from 'sharp';
 import * as fetch from 'node-fetch';
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const {img, r, w, h, f, pos, bg, we} = req.query
+  const {img, r, w, h, fit, pos, bg, we} = req.query
 
   if (!img) return res.status(400).json({code: 400, error: 'img param with url missing.'})
 
   /**
    * Normalize params
    */
-  const fit = f || 'cover'
   const background = bg ? `#${bg}` : '#00000000'
   const position = String(pos).replace(',', ' ') || 'center'
   const withoutEnlargement = Boolean(we)
@@ -25,7 +24,7 @@ export default async (req: NowRequest, res: NowResponse) => {
       Number(w) || undefined,
       Number(h) || undefined,
       {
-        fit,
+        fit: fit || 'cover',
         position,
         background,
         withoutEnlargement,
