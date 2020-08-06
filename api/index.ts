a3414dec20444c79e1f81df477c076eefbfa6cc9
Juan Pujol <jp@beakyn.com>
2020-08-05 22:06:05 -0300

    feat(api): add support for custom format and quality

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import sharp from 'sharp';
 import * as fetch from 'node-fetch';
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const {img, r, w, h, fit, pos, bg, withoutEnlargement} = req.query
+  const {img, r, w, h, fit, pos, bg, withoutEnlargement, format, q} = req.query
 
   if (!img) return res.status(400).json({code: 400, error: 'img param with url missing.'})
 
@@ -32,12 +32,20 @@ export default async (req: NowRequest, res: NowResponse) => {
 
     if (r) sharpResponse = sharpResponse.rotate(Number(r) || 0, { background })
 
-    sharpResponse = await sharpResponse.png().toBuffer()
+    if (format) {
+      sharpResponse = await sharpResponse
+        .toFormat(format, {quality: Number(q) || 80})
+        .toBuffer({resolveWithObject: true});
+    } else {
+      sharpResponse = await sharpResponse.toBuffer({resolveWithObject: true});
+    }
 
-    res.setHeader('Content-Type', 'image/png')
-    res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
+    const {data, info} = sharpResponse;
 
-    res.send(sharpResponse)
+    res.setHeader('Content-Type', `image/${info.format}`)
+    res.setHeader('Cache-Control', 'max-age=2592000, s-maxage=21600, stale-while-revalidate=86400, public')
+
+    res.send(data)
   } catch (error) {
     throw new Error(error);
   }
